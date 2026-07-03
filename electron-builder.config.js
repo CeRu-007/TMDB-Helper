073316c9879a062b66c251ab9ecf55d6eea3e42d
CeRu-007 <481646650@qq.com>
2026-07-03 23:11:27 +0800

    build(electron-builder): 重构产物命名与构建配置
    统一通过配置文件管理各平台产物命名规则，添加构建后缀区分普通/浏览器模式，所有构建命令统一使用指定配置文件

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -2,12 +2,22 @@ const packageJson = require('./package.json');
 
 const isBrowser = process.env.ELECTRON_BROWSER_MODE === 'true';
 const baseConfig = packageJson.build || {};
+const suffix = isBrowser ? '-web' : '';
 
 module.exports = {
   ...baseConfig,
-  artifactName: isBrowser
-    ? '${productName}-${version}-${platform}-${arch}-web.${ext}'
-    : '${productName}-${version}-${platform}-${arch}.${ext}',
+  mac: {
+    ...(baseConfig.mac || {}),
+    artifactName: `\${productName}-\${version}-mac-\${arch}${suffix}.\${ext}`,
+  },
+  win: {
+    ...(baseConfig.win || {}),
+    artifactName: `\${productName}-\${version}-win-\${arch}${suffix}.\${ext}`,
+  },
+  linux: {
+    ...(baseConfig.linux || {}),
+    artifactName: `\${productName}-\${version}-linux-\${arch}${suffix}.\${ext}`,
+  },
   extraMetadata: {
     ...(baseConfig.extraMetadata || {}),
     main: isBrowser
