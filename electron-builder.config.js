eb0952008f7a00eb2a2f19d48dec8f3c05355c50
CeRu-007 <481646650@qq.com>
2026-07-03 22:50:57 +0800

    build(ci): 新增浏览器模式的Electron构建配置和CI工作流
    支持在Windows、macOS和Linux平台编译浏览器版本的安装包，并将其纳入release打包流程

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -1,12 +1,17 @@
 const packageJson = require('./package.json');
 
+const isBrowser = process.env.ELECTRON_BROWSER_MODE === 'true';
+const baseConfig = packageJson.build || {};
+
 module.exports = {
-  ...packageJson.build,
+  ...baseConfig,
+  artifactName: isBrowser
+    ? '${productName}-${version}-${platform}-${arch}-web.${ext}'
+    : '${productName}-${version}-${platform}-${arch}.${ext}',
   extraMetadata: {
-    ...(packageJson.build?.extraMetadata || {}),
-    main:
-      process.env.ELECTRON_BROWSER_MODE === 'true'
-        ? 'electron/tray-browser.js'
-        : (packageJson.build?.extraMetadata?.main ?? 'electron/main.js'),
+    ...(baseConfig.extraMetadata || {}),
+    main: isBrowser
+      ? 'electron/tray-browser.js'
+      : (baseConfig.extraMetadata?.main ?? 'electron/main.js'),
   },
 };
